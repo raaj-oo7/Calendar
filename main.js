f035caf5d2e2e56790d1e7e2c6c41e5dfed08003
raaj-oo7 <raj.makadia22@gmail.com>
2023-11-02 18:24:20 +0530

    update code and refactor code

--- main.js
+++ main.js
@@ -1,46 +1,34 @@
 let month = 0;
 
-const prevMonthButton = document.getElementById("prevMonth");
+const previousMonthButton = document.getElementById("prevMonth");
 const nextMonthButton = document.getElementById("nextMonth");
 const currentMonthYear = document.getElementById("currentMonthYear");
 const calendarDays = document.getElementById("calendarDays");
-const themeSwitch = document.getElementById('checkbox');
+const themeSwitch = document.getElementById("checkbox");
 const body = document.body;
-let isDarkTheme = false;
+const isDarkTheme = false;
 
 // Functions
-function updateCalendar() {
-    let date = new Date();
+function getPreviousMonthDays(date) {
+    const lastDayOfPreviousMonth = new Date(date.getFullYear(), date.getMonth(), 0);
+    return lastDayOfPreviousMonth.getDate();
+}
 
-    date.setMonth(date.getMonth() + month);
-
-    // Set the displayed month and year
-    currentMonthYear.textContent = `${date.toLocaleString("en-US", { month: "long" })} ${date.getFullYear()}`;
-
-    calendarDays.innerHTML = '';
-    // Calculate the first day of the month
-    let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-
-    // Create the previous month's days
-    let prevMonthLastDay = new Date(date.getFullYear(), date.getMonth(), 0);
-    let prevMonthDays = prevMonthLastDay.getDate();
-
+function createPreviousMonthDays(prevMonthDays, firstDay) {
     for (let i = firstDay.getDay(); i > 0; i--) {
         const day = document.createElement("li");
         day.textContent = prevMonthDays - i + 1;
         day.classList.add("date-inactive");
         calendarDays.appendChild(day);
     }
+}
 
-    // Create the current month's days
+function createCurrentMonthDays(lastDay, date) {
     for (let i = 1; i <= lastDay.getDate(); i++) {
-
         const day = document.createElement("li");
         day.classList.add("active");
         day.textContent = i;
 
-        // Check the current day is today
         if (
             date.getFullYear() === new Date().getFullYear() &&
             date.getMonth() === new Date().getMonth() &&
@@ -51,14 +39,40 @@ function updateCalendar() {
         }
         calendarDays.appendChild(day);
     }
+}
 
-    // Create the next month's days
+function createNextMonthDays() {
     for (let i = 1; calendarDays.children.length < 42; i++) {
         const day = document.createElement("li");
         day.textContent = i;
         day.classList.add("date-inactive");
         calendarDays.appendChild(day);
     }
+}
+
+function createCalendarTotalDays(date) {
+    calendarDays.innerHTML = "";
+
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+    const previousMonthDays = getPreviousMonthDays(date);
+
+    createPreviousMonthDays(previousMonthDays, firstDay);
+    createCurrentMonthDays(lastDay, date);
+    createNextMonthDays();
+}
+
+function updateCalendar() {
+    const date = new Date();
+
+    date.setMonth(date.getMonth() + month);
+
+    // Set the displayed month and year
+    currentMonthYear.textContent = `${date.toLocaleString("en-US", { month: "long" })} ${date.getFullYear()}`;
+
+    // Populate the calendar with days
+    createCalendarTotalDays(date);
 }
 updateCalendar();
 
@@ -72,17 +86,17 @@ if (cachedTheme) {
 
 // Function to toggle between light and dark themes
 function toggleTheme() {
-    if (body.dataset.theme === 'light') {
-        body.dataset.theme = 'dark';
-        localStorage.setItem('theme', 'dark');
+    if (body.dataset.theme === "light") {
+        body.dataset.theme = "dark";
+        localStorage.setItem("theme", "dark");
     } else {
-        body.dataset.theme = 'light';
-        localStorage.setItem('theme', 'light');
+        body.dataset.theme = "light";
+        localStorage.setItem("theme", "light");
     }
 }
 
 // Event listeners for previous and next month buttons
-prevMonthButton.addEventListener("click", function () {
+previousMonthButton.addEventListener("click", function () {
     month--;
     updateCalendar();
 });
@@ -92,10 +106,10 @@ nextMonthButton.addEventListener("click", function () {
     updateCalendar();
 });
 // Set the initial theme switch based on the cached theme
-themeSwitch.checked = body.dataset.theme === 'dark';
+themeSwitch.checked = body.dataset.theme === "dark";
 
 // Event listener for toggle theme button
 themeSwitch.addEventListener("click", function (e) {
     toggleTheme();
-    themeSwitch.checked = body.dataset.theme === 'dark';
+    themeSwitch.checked = body.dataset.theme === "dark";
 });
